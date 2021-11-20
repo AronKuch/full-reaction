423e90f152f7c0ac0bb249fd5babe99a725b5fe5
Aron Kuch <AronKuch@gmail.com>
2021-11-20 13:06:33 -0500

    Artilce list component and page updates

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import content from "./content.js";
 import ReactHtmlParser from "react-html-parser";
+import content from "./content.js";
+import ListComponent from "../components/listcomponent.js";
 
 const ArticlePage = ({ match }) => {
   let { name } = useParams();
   let article = content.find((article) => article.name === name);
+  let otherArticles = content.filter((article) => article.name !== name);
   return (
     <>
       <h1>{article.title}</h1>
       {ReactHtmlParser(article.content)}
+      <h2>Other Article</h2>
+      <ListComponent articles={otherArticles} />
     </>
   );
 };
--- src/pages/articleslist.js
+++ src/pages/articleslist.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ListComponent from "../components/listcomponent.js";
+import content from "./content.js";
 
 const ArticleListPage = () => (
   <>
     <h1>Articles</h1>
-    <p>This page will have a list of articles.</p>
+    <ListComponent articles={content} sample={true} />
   </>
 );
 
